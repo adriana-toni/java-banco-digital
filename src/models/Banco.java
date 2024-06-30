208db955904399e6d23a659a70c9da683c12398e
adriana.toni <adriana.toni@gmail.com>
2024-06-30 13:04:19 -0300

    POO: Encapsulamento

--- src/models/Banco.java
+++ src/models/Banco.java
@@ -1,6 +1,23 @@
 package models;
 
 public class Banco {
-    public int numeroBanco;
-    public String nomeBanco;
+    private int numeroBanco;
+    private String nomeBanco;
+
+    public Banco(int numeroBanco, String nomeBanco) {
+        this.numeroBanco = numeroBanco;
+        this.nomeBanco = nomeBanco;
+    }
+
+    public int getNumeroBanco() {
+        return numeroBanco;
+    }
+
+    public String getNomeBanco() {
+        return nomeBanco;
+    }
+
+    public void setNomeBanco(String nomeBanco) {
+        this.nomeBanco = nomeBanco;
+    }
 }
--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,6 +1,13 @@
 package models;
 
 public class Cliente {
-    public String nomeCliente;
+    private String nomeCliente;
 
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
 }
